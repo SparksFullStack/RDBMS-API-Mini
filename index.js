5b09c94dba3f2b5ea9633eb551cb59790771597a
SparksFullStack <csparks@antioch.edu>
2018-08-08 12:55:58 -0700

    post and get to '/zoos' working

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require("./knexfile");
 
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 
 // endpoints here
+
+server.post("/zoos", (req, res) => {
+  const newZoo = req.body;
+
+  db.insert(newZoo)
+    .into("Zoos")
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err));
+})
+
+server.get('/zoos', (req, res) => {
+  db.select('name')
+    .from('zoos')
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json(err));
+})
 
 const port = 3300;
 server.listen(port, function() {
